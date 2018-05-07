41012417b9f1075bf80c6120a2a7fa63b015f038
zzl <384719678@qq.com>
2018-05-07 16:22:08 +0800

    test

--- ScreenLock/app/src/main/java/com/jikexueyuan/screenlock/GestureLock.java
+++ ScreenLock/app/src/main/java/com/jikexueyuan/screenlock/GestureLock.java
@@ -1,4 +1,4 @@
-package com.jikexueyuan.screenlock;
+﻿package com.jikexueyuan.screenlock;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -9,6 +9,7 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+
 import android.view.View;
 
 import java.util.ArrayList;
